d6514d1c1ddeb8561de0c4ca69087132210fa851
TravelerVihaan <lukasz.biel@10g.pl>
2022-02-26 15:04:36 +0100

    First commit

---
+++ src/test/java/com/github/travelervihaan/hogwarts/noticeboard/HogwartsNoticeboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.travelervihaan.hogwarts.noticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsNoticeboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
